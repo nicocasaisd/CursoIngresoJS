0303a4733636e59c45e42edac0bf5bdbf0fa0f31
nicocasaisd <nicolas.casais.dassie@gmail.com>
2022-02-09 11:31:32 -0300

    Clase 5

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -7,8 +7,99 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
+Casais Dassie, Nicolás
+TP 4 - FerreteIluminación
+punto A+B+C+D+E
  */
 function CalcularPrecio () 
 {
- 	
+ 	// Declaro variables
+    let cantidadLamparas;
+    let marcaLamparas;
+    let precioBase;
+    let precioDescuento;
+    let precioFinal;
+    let ingresosBrutos;
+    let descuento = 0;
+    let precioUnidad = 35;
+
+
+    // Obtengo los valores por ID
+    cantidadLamparas = document.getElementById('txtIdCantidad').value;
+    cantidadLamparas = parseInt(cantidadLamparas);
+
+    marcaLamparas = document.getElementById('Marca').value;
+
+    // Calculo el porcentaje de descuento
+    if(cantidadLamparas > 5)
+    {
+        descuento = 0.5;
+    }
+    else
+    {
+        if(cantidadLamparas == 5)
+        {
+            if(marcaLamparas == "ArgentinaLuz")
+            {
+                descuento = 0.4;
+            }
+            else
+            {
+                descuento = 0.3;
+            }
+        }
+        else
+        {
+            if(cantidadLamparas == 4)
+            {
+                if(marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas")
+                {
+                    descuento = 0.25;
+                }
+                else
+                {
+                    descuento = 0.2;
+                }
+            }
+            else
+            {
+                if(cantidadLamparas == 3)
+                {
+                    if(marcaLamparas == "ArgentinaLuz")
+                    {
+                        descuento = 0.15;
+                    }
+                    else
+                    {
+                        if(marcaLamparas == "FelipeLamparas")
+                        {
+                            descuento = 0.1;
+                        }
+                        else
+                        {
+                            descuento = 0.05;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Calculo el precio
+    precioBase = cantidadLamparas * precioUnidad;
+    precioDescuento = precioBase * descuento;
+    precioFinal = cantidadLamparas * precioUnidad - precioDescuento;
+
+    // Calculo si debe aplicar ingresos brutos
+    if(precioFinal > 120)
+    {
+        ingresosBrutos = precioFinal * 0.1;
+        precioFinal = precioFinal + ingresosBrutos;
+        alert("IIBB: Usted pagó $" + ingresosBrutos);
+    }
+
+    // Muestro el resultado
+    document.getElementById('txtIdprecioDescuento').value = precioFinal;
+
 }
